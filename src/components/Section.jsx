8243424c9b97d965efd81188f2707b5929718b71
BenMiriello <benmiriello@gmail.com>
2020-04-24 16:15:05 -0400

    add :after style to h2

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -7,8 +7,10 @@ const Section = (props) => {
       <Separator px="20"/>
       <div className="main-section">
         <h2>{props.title}</h2>
+        <Separator px="20" />
         <div className="narrow">{props.children}</div>
       </div>
+      <Separator px="20"/>
     </>
   );
 };
